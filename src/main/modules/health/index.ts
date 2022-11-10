13c70dc0e0cb390b1388fa2e2a6914eefa3134db
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-11-10 18:56:23 +0000

    SIDM-8223 - Replaced health route with nodejs-healthcheck middleware + configured

---
+++ src/main/modules/health/index.ts
@@ -0,0 +1,31 @@
+import os from 'os';
+import healthcheck from '@hmcts/nodejs-healthcheck';
+import config from 'config';
+import { Application } from 'express';
+
+export class HealthCheck {
+  public enableFor(app: Application): void {
+    const healthOptions = () => ({
+      timeout: config.get('health.timeout'),
+      deadline: config.get('health.deadline'),
+    });
+
+    // Services
+    const idamApi = healthcheck.web(config.get('services.idam.url.api') + '/health', healthOptions);
+    const redis = healthcheck.raw(() => app.locals.redisClient.ping() ? healthcheck.up() : healthcheck.down())
+
+    const checks = {
+      idamApi,
+      ...(app.locals.redisClient && { redis })
+    };
+
+    healthcheck.addTo(app, {
+      checks,
+      buildInfo: {
+        name: config.get('services.name'),
+        host: os.hostname(),
+        uptime: process.uptime(),
+      }
+    });
+  }
+}
