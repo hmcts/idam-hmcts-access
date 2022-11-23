31577b2cf6b0ee161d85c44e76e7f09d4f93a662
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-11-23 19:40:05 +0000

    Cleanup imports

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,14 +1,11 @@
 #!/usr/bin/env node
-import * as fs from 'fs';
-import * as https from 'https';
-import * as path from 'path';
-
+import fs from 'fs';
+import https from 'https';
+import path from 'path';
 import { app } from './app';
-
-const { Logger } = require('@hmcts/nodejs-logging');
+import { Logger } from '@hmcts/nodejs-logging';
 
 const logger = Logger.getLogger('server');
-
 const port: number = parseInt(process.env.PORT || '3100', 10);
 
 if (app.locals.developmentMode) {
@@ -17,6 +14,7 @@ if (app.locals.developmentMode) {
     cert: fs.readFileSync(path.join(sslDirectory, 'localhost.crt')),
     key: fs.readFileSync(path.join(sslDirectory, 'localhost.key')),
   };
+
   const server = https.createServer(sslOptions, app);
   server.listen(port, () => {
     logger.info(`Application started: https://localhost:${port}`);
