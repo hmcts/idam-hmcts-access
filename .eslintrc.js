001507868aff6e5d3140304bafcd988d1c0ab672
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-11-29 13:33:35 +0000

    SIDM-8262 - Updated linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  ignorePatterns: ['src/main/views/govuk/**'],
+  ignorePatterns: ['src/main/steps/govuk/**'],
   env: { browser: true, es6: true, node: true },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'import', 'jest'],
